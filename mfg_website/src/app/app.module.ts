38bc5fe0c7f11d97bcd7a0d8a7927823f4cc4d69
Andrew Alagna <aalagna04@gmail.com>
2021-11-30 08:08:22 -0500

    remove inmemorydb

--- mfg_website/src/app/app.module.ts
+++ mfg_website/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api'; //delete this when actual api is added
-// import { InMemoryDataService } from '../inmemoryAPI/in-memory-data.service';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -34,13 +33,6 @@ import { GetUserComponent } from './user/get-user/get-user.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-
-    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
-    // and returns simulated server responses.
-    // Remove it when a real server is ready to receive requests.
-    // HttpClientInMemoryWebApiModule.forRoot(
-    //   InMemoryDataService, { dataEncapsulation: false }
-    // )
   ],
   providers: [SharedService],
   bootstrap: [AppComponent]
