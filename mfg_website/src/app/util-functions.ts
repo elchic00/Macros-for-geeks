7164135ff43dd6c677bc50af9b131139e66a16eb
CoffeeWarrior <lionlikelucas@gmail.com>
2021-11-29 22:22:45 -0500

    added util function

---
+++ mfg_website/src/app/util-functions.ts
@@ -0,0 +1,29 @@
+import { Food } from "./food";
+
+// export interface Food {
+//     readonly Food:string,
+//     readonly Calories:number,
+//     readonly Carbohydrates:number,
+//     readonly Fats:number,
+//     readonly Protein:number,
+//     readonly Date:Date,
+//     readonly Mealtime:string,
+//     readonly UserId?:number
+// }
+
+export function totalNutritionDay(foods:Food[]):any{ //returns calories, carbohydrates, fats, protiens of array added up
+    let results = {
+        Calories: 0,
+        Carbohydrates: 0,
+        Fats: 0,
+        Protien: 0
+    }
+    for(const food of foods){
+        let {Calories, Carbohydrates, Fats, Protein} = food;
+        results.Calories += Calories;
+        results.Carbohydrates += Carbohydrates;
+        results.Fats += Fats;
+        results.Protien += Protein;
+    }
+    return results;
+}
