c397e62bd6788495d1dceab8037e8fd52f0b9fad
Andrew Alagna <aalagna04@gmail.com>
2021-12-10 13:43:30 -0500

    add get user service to login
    still needs to user the get request in the front end to pick user

--- mfg_website/src/app/login/login.component.ts
+++ mfg_website/src/app/login/login.component.ts
@@ -1,15 +1,23 @@
+import { _getFocusedElementPierceShadowDom } from '@angular/cdk/platform';
 import { Component, OnInit } from '@angular/core';
 import { FoodinputComponent } from '../foodinput/foodinput.component';
-
+import { SharedService } from '../services/shared.service';
+import { User } from '../user/user';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+users : User[] = []
+  constructor(private sharedService: SharedService) { }
 
   ngOnInit(): void {
+      this.getUsers();
   }
+
+  getUsers(){
+    this.sharedService.getUsers().subscribe(user => this.users = user)
+  }
+  
 }
