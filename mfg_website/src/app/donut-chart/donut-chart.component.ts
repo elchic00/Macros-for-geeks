34574d17833e3e221240af6e3cd67439d354136b
Andrew Alagna <aalagna04@gmail.com>
2021-12-28 10:17:31 -0500

    add labels to doughnut chart and update gif

--- mfg_website/src/app/donut-chart/donut-chart.component.ts
+++ mfg_website/src/app/donut-chart/donut-chart.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ChartType } from 'chart.js';
-import {MultiDataSet, Label, SingleDataSet} from 'ng2-charts';
+import {MultiDataSet, Label, SingleOrMultiDataSet} from 'ng2-charts';
 import {SharedService} from "../services/shared.service";
-import { Food } from '../interfaces/food';
+
 @Component({
   selector: 'doughnut',
   templateUrl: './donut-chart.component.html',
@@ -11,15 +11,12 @@ import { Food } from '../interfaces/food';
 export class DonutChartComponent implements OnInit {
   date : Date = new Date();
   food : any = []
-  protien : number = 0
-  fat:number = 0
-  carbs : number = 0
-  data : number[] = [0,0,0]
+  macros : number[] = [0,0,0]
 
   // Doughnut
   public doughnutChartLabels: Label[] = ['Protein', 'Fat', 'Carbohydrate'];
-  public doughnutChartData: MultiDataSet = [
-    this.data
+  public doughnutChartData: SingleOrMultiDataSet /*Array<number[]>*/ = [
+    this.macros
   ];
   public doughnutChartType: ChartType = 'doughnut';
 
@@ -30,16 +27,22 @@ export class DonutChartComponent implements OnInit {
     this.sharedService.getDiaryByDate(this.userId,this.date).subscribe(entries => {
       /*console.log(entries)*/
       for (var i = 0; i < entries.length ;i++){
-        this.data[0] += entries[i].protein
-        this.data[1] += entries[i].fats
-        this.data[2] += entries[i].carbohydrates
-        console.log(entries[i])
-        console.log(this.data)
-      };
-      //const resArr = entries.Food
-      this.food.push(entries)  ;})
+        this.macros[0] += entries[i].protein
+        this.macros[1] += entries[i].fats
+        this.macros[2] += entries[i].carbohydrates
+      }
+    })
+    console.log(this.macros)
+  }
 
-    console.log(this.food)
+  public chartOptions: any = {
+    pieceLabel: {
+      render: function (args: any) {
+        const label = args.label,
+          value = args.value;
+        return label + ': ' + value;
+      }
+    }
   }
 
   get userId():number {
