9097d323541b9fb0d8019fe8b4ad50a9a7a8b824
Andrew Alagna <aalagna04@gmail.com>
2021-12-28 12:01:21 -0500

    labels and styling to progress chart

--- mfg_website/src/app/progress/progress.component.ts
+++ mfg_website/src/app/progress/progress.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import {DonutChartComponent} from "../donut-chart/donut-chart.component";
 import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
 import { MultiDataSet, Label } from 'ng2-charts';
+import { DatePipe } from '@angular/common';
+import {SharedService} from "../services/shared.service";
 
 @Component({
   selector: 'app-progress',
@@ -10,38 +12,60 @@ import { MultiDataSet, Label } from 'ng2-charts';
   styleUrls: ['./progress.component.css']
 })
 export class ProgressComponent implements OnInit {
-  private pastWeek : Date[] = [];
-  
+  private pastWeek : string[] = [];
+  date : Date = new Date();
+  protiens : number[] = [0,0,0,0,0,0,0]
+  fats : number[] = [0,0,0,0,0,0,0]
+  carbs: number[] = [0,0,0,0,0,0,0]
+
   public barChartOptions: ChartOptions = {
     responsive: true,
   };
-  
-  public barChartLabels: Label[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-  
+
+  public barChartLabels: any = this.pastWeek;
+
   public barChartType: ChartType = 'bar';
   public barChartLegend = true;
   public barChartPlugins = [];
 
   public barChartData: ChartDataSets[] = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' }
+    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Protiens' },
+    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Fats' },
+    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Carbohydrates' }
+
   ];
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,private datePipe: DatePipe,private sharedService: SharedService) { }
 
   ngOnInit(){
+    this.sharedService.getDiaryByDate(this.userId,this.date).subscribe(entries => {
+      for (var i = 0; i < entries.length ;i++){
+        this.protiens[0] += entries[i].protein
+      }
+    })
+    this.sharedService.getDiaryByDate(this.userId,this.date).subscribe(entries => {
+      for (var i = 0; i < entries.length ;i++){
+        this.fats[1] += entries[i].fats
+
+      }
+    })
     // this.http.post<any>('', {}).subscribe(data => {
     // })
-    
     for(let i = 0; i < 7; i++){
       let currentDate: Date = new Date();
       currentDate.setDate(currentDate.getDate() - i);
-      this.pastWeek.push(currentDate);
-      console.log(currentDate.getDate());
-      
-    }
+      this.pastWeek.push(currentDate.toDateString());
+      console.log(currentDate);
 
+    }
       this.pastWeek.reverse();
+  }
+
+
+
+  get userId():number {
+    console.log("userID is:" + this.sharedService.userId)
+    return this.sharedService.userId
   }
 
 }
